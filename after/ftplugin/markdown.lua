71833aa16cc97c1327b6eb601fa9106fc7d41815
srdusr <trevorgray@srdusr.com>
2024-03-01 17:21:36 +0200

    Add nvim-surround settings for markdown

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -7,6 +7,7 @@ vim.wo.colorcolumn = "0"
 vim.wo.conceallevel = 3
 vim.opt.softtabstop = 2 -- Tab key indents by 2 spaces.
 vim.opt.shiftwidth = 2  -- >> indents by 2 spaces.
+-- vim.g.markdown_recommended_style = 0 -- prevents markdown from changing tabs to 4 spaces
 
 vim.b[0].undo_ftplugin = "setlocal nospell nowrap nolinebreak nobreakindent conceallevel=0"
 
@@ -14,3 +15,23 @@ vim.cmd([[
   autocmd FileType markdown iabbrev <buffer> `` ``
 ]])
 
+require("nvim-surround").buffer_setup({
+  surrounds = {
+    -- ["e"] = {
+    --   add = function()
+    --     local env = require("nvim-surround.config").get_input ("Environment: ")
+    --     return { { "\\begin{" .. env .. "}" }, { "\\end{" .. env .. "}" } }
+    --   end,
+    -- },
+    ["b"] = {
+      add = { "**", "**" },
+      find = "**.-**",
+      delete = "^(**)().-(**)()$",
+    },
+    ["i"] = {
+      add = { "_", "_" },
+      find = "_.-_",
+      delete = "^(_)().-(_)()$",
+    },
+  },
+})
